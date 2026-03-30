290bc2aea0ba9ddd4bac4e2f7f9ad416d07fd649
premkumar9988 <premkumar00786337@gmail.com>
2026-03-30 08:10:06 -0300

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black text-white text-center p-4 mt-10">
-      <p>© 2026 Premium Bookstore. All rights reserved.</p>
-    </footer>
-  );
-}
